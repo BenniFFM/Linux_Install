365df72bee148267f0a7e6a8bfe9ecbb7f3c0636
BenniFFM <87622659+BenniFFM@users.noreply.github.com>
2021-07-31 18:12:21 +0200

    Update Debian_SSH_conf.sh

--- Debian_SSH_conf.sh
+++ Debian_SSH_conf.sh
@@ -15,4 +15,4 @@ if [ "$CHANGE" = "y" ]; then {
   rm -v /etc/ssh/ssh_host_*
   sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config
   dpkg-reconfigure openssh-server
-}fi
+} fi
