2f621c6206da164c7b54f09d2a377d575ec6e691
BenniFFM <87622659+BenniFFM@users.noreply.github.com>
2021-07-31 11:08:59 +0200

    Update and rename ZSH_install.sh to Debian_ZSH_install.sh

---
+++ Debian_ZSH_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Install and configure zsh on debian systems
+# Last Change: 31.07.2021
+# File History:
+# 31.07.2021	Inital Release
+#
+#
+#wget --no-check-certificate https://raw.githubusercontent.com/BenniFFM/Linux_Install/main/Debian_ZSH_install.sh && chmod +x ./Debian_ZSH_install.sh
+####################### ZSH #######################
+# Install 
+echo "---------------------------------------------------------------------------------"
+echo -n "Do you want to install 'ZSH' (y/N) "
+read CHANGE
+if [ "$CHANGE" = "y" ]; then {
+  apt-get -y install zsh
+  cd /root
+  wget -O --no-check-certificate .zshrc https://raw.githubusercontent.com/BenniFFM/Linux_Install/main/etc_zsh_zshrc
+  find /home -maxdepth 1 -type d -exec sh -c 'cp /root/.zshrc "$0" -P' {} \;
+  sed -i "s/bash/zsh/" /etc/passwd
+} fi
--- ZSH_install.sh
+++
@@ -1,6 +0,0 @@
-apt-get -y install zsh
-cd /root
-wget -O .zshrc http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
-#cd /home/pi
-#wget -O .zshrc http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
-sed -i "s/bash/zsh/" /etc/passwd
